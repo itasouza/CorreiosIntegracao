d5b04329fb911d5ee49ce399006d6de79ddff2cd
itasouza <itasouza@yahoo.com.br>
2017-05-25 00:33:38 -0300

    Integração com Correios
    Integração com WebService dos correios

---
+++ Projeto.Web.Correios/Projeto.Web.Correios/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Correios;
+
+namespace Projeto.Web.Correios.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> Index(string cep)
+        {
+            var service = new CorreiosApi();
+
+            try
+            {
+                var dados = (await service.consultaCEPAsync(cep)).@return;
+
+                if (dados.cep != "")
+                {
+                    string endereco = dados.end;
+                    string bairro = dados.bairro;
+                    string cidade = dados.cidade;
+                    string cepRetorno = dados.cep;
+                    string complemento = dados.complemento;
+                    string complemento2 = dados.complemento2;
+                    string estado = dados.uf;
+
+                    TempData["endereco"] = endereco;
+                    TempData["bairro"] = bairro;
+                    TempData["cidade"] = cidade;
+                    TempData["cep"] = cepRetorno;
+                    TempData["complemento"] = complemento;
+                    TempData["complemento2"] = complemento2;
+                    TempData["estado"] = estado;
+                }
+                else
+                {
+                    TempData["erro"] = "erro";
+                }
+
+                return View();
+            }
+            catch (Exception)
+            {
+                TempData["erro"] = "erro";
+                return View();
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Projeto.Web.Correios/Projeto.Web.Correios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto.Web.Correios.MvcApplication" Language="C#" %>
